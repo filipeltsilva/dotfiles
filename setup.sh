ee69827023e906dc037ab58bcf1f0131b3dd58ff
Filipe Lemos <contato@filipelemos.com>
2015-04-20 02:24:16 -0300

    Adding zsh support

--- setup.sh
+++ setup.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-rm $HOME/.bashrc
-ln -s $HOME/.dotfiles/bashrc $HOME/.bashrc
+echo
+echo 'Instalando o e ativando o oh-my-zsh'
+curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+echo
+sleep 3
+
+echo 'Configurando o zsh'
+rm $HOME/.zshrc
+ln -s $HOME/.dotfiles/zshrc $HOME/.zshrc
+
+# rm $HOME/.bashrc
+# ln -s $HOME/.dotfiles/bashrc $HOME/.bashrc
