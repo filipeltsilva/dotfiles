df725e940fb3b791c46b1a9cca46e24aeed5bfd9
Filipe Lemos <contato@filipelemos.com>
2016-10-28 23:14:26 -0200

    Creating the pre_install script

---
+++ pre_install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# SCRIPT NAME: remote_install
+# CREATION DATE: 2016-10-28
+#
+#
+# DESCRIPTION: Runs the dotfiles pre-installation routine, downloading the repository without using Git
+#
+# AUTHOR: Filipe Lemos <contato@filipelemos.com>
+# AUTHOR URL: filipelemos.com
+
+cd $HOME
+mkdir -p $HOME/.dotfiles
+curl -#L https://github.com/filipeltsilva/dotfiles/tarball/master > dotfiles.tar.gz
+tar -zxvf files.tar.gz -C $HOME/dotfailes --strip-components=1
+rm -f dotfiles.tar.gz
+. $HOME/.dotfiles/install.sh
