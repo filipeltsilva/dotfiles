70b7e9120b20f4fb91c7d9814047ebb5f915cda4
Filipe Lemos <contato@filipelemos.com>
2020-11-07 22:33:19 -0300

    Refactoring macOS, Vim and ZSH installers

---
+++ zsh/install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+echo "ZSH SETUP"
+sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+echo "Creating links"
+ln -sfnv $PWD/.zshrc $HOME/.zshrc
+ln -sfnv $PWD/.zprofile $HOME/.zprofile
+
+echo "Setting ZSH as default shell"
+chsh -s $(which zsh) $USER
