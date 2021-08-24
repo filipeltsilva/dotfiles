4eddce073ec2cab652d77c5862840ffd0f83fc67
Filipe Lemos <contato@filipelemos.com>
2021-08-24 20:53:10 -0300

    Refactoring shell paths

--- zsh/paths.zsh
+++ zsh/paths.zsh
@@ -1,11 +1,3 @@
 # export MANPATH="/usr/local/man:$MANPATH"
 
-# ENVIRONMENT VARIABLES
-if [[ `uname` == "Darwin" ]]; then
-  export MANPATH="${MANPATH}:$(brew --prefix coreutils)/libexec/gnuman:$(brew --prefix findutils)/libexec/gnuman"
-  export PATH="${PATH}:$(brew --prefix coreutils)/libexec/gnubin:$(brew --prefix findutils)/libexec/gnubin:/usr/local/sbin:${HOME}/.dotfiles/bin"
-else
-  export PATH="${PATH}:${HOME}/.dotfiles/bin"
-fi
-
-#export PATH="$(yarn global bin):$PATH"
+export PATH=$PATH:$(yarn global bin):$HOME/.local/bin:$HOME/.dotfiles/bin
