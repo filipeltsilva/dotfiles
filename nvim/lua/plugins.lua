eaaa3fded2a4f5f76027f10590fbe631f48b3f75
Filipe Lemos <830213+filipeltsilva@users.noreply.github.com>
2022-05-06 21:40:25 -0300

    Adding Neovim plugins

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -2,5 +2,7 @@ vim.cmd [[ packadd packer.nvim ]]
 
 return require('packer').startup(function()
   use 'airblade/vim-gitgutter'
+  use 'gpanders/editorconfig.nvim'
+  use 'max-0406/autoclose.nvim'
   use 'wbthomason/packer.nvim'
 end)
