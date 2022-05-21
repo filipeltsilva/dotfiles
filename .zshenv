7f546b3f87f30f1d9f826496ea1cbe164546f850
Filipe Lemos <830213+filipeltsilva@users.noreply.github.com>
2022-05-21 17:46:23 -0300

    Improving asdf-vm and Neovim plugins

--- .zshenv
+++ .zshenv
@@ -11,7 +11,7 @@ export ASDF_DIR="/opt/asdf-vm"
 export ASDF_DATA_DIR="$XDG_DATA_HOME/asdf"
 export ASDF_CONFIG_FILE="$XDG_CONFIG_HOME/asdf/asdfrc"
 export ASDF_DEFAULT_TOOL_VERSIONS_FILENAME="$XDG_CONFIG_HOME/asdf/.tool-versions"
-export ASDF_GEM_DEFAULT_PACKAGES_FILER="$XDG_CONFIG_HOME/asdf/.default-gems"
+export ASDF_GEM_DEFAULT_PACKAGES_FILE="$XDG_CONFIG_HOME/asdf/.default-gems"
 export ASDF_NPM_DEFAULT_PACKAGES_FILE="$XDG_CONFIG_HOME/asdf/.default-npm-packages"
 
 export NPM_CONFIG_USERCONFIG="$XDG_CONFIG_HOME/npm/npmrc"
--- nvim/lua/assets/plugins.lua
+++ nvim/lua/assets/plugins.lua
@@ -3,15 +3,18 @@ vim.cmd 'packadd packer.nvim'
 return require('packer').startup(function()
   use'wbthomason/packer.nvim'
 
-  use 'airblade/vim-gitgutter'
-  use 'gpanders/editorconfig.nvim'
-  use 'max-0406/autoclose.nvim'
+  use 'navarasu/onedark.nvim'
+
+  use 'nvim-lualine/lualine.nvim'
 
   use {
-    'nvim-lualine/lualine.nvim',
+    'kyazdani42/nvim-tree.lua',
     requires = { 'kyazdani42/nvim-web-devicons', opt = true }
   }
 
-  use 'navarasu/onedark.nvim'
-end)
+  use 'neovim/nvim-lspconfig'
 
+  use 'airblade/vim-gitgutter'
+  use 'gpanders/editorconfig.nvim'
+  use 'max-0406/autoclose.nvim'
+end)
