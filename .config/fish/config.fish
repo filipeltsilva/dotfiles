3316a4de92924106037438858b2b0881528ebce9
Filipe Lemos <830213+filipeltsilva@users.noreply.github.com>
2023-09-04 11:18:47 -0300

    Cleaning a little mess

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,13 +8,8 @@ set -gx XDG_STATE_HOME $HOME/.local/state
 set -gx EDITOR nvim
 set -gx NPM_CONFIG_USERCONFIG $XDG_CONFIG_HOME/npm/npmrc
 
-#set -gx ASDF_CONFIG_FILE $XDG_CONFIG_HOME/asdf/asdfrc
-#set -gx ASDF_DATA_DIR $XDG_DATA_HOME/asdf
-#set -gx ASDF_DEFAULT_TOOL_VERSIONS_FILENAME $XDG_CONFIG_HOME/asdf/.tool-versions
-#set -gx ASDF_DIR /opt/asdf-vm
 set -gx RTX_NODE_DEFAULT_PACKAGES_FILE $XDG_CONFIG_HOME/rtx/.default-npm-packages
 set -gx RTX_RUBY_DEFAULT_PACKAGES_FILE $XDG_CONFIG_HOME/rtx/.default-gems
-#set -gx ASDF_PLUGIN_MANAGER_VERSIONS_FILENAME $XDG_CONFIG_HOME/.plugin-versions
 
 $XDG_DATA_HOME/rtx/bin/rtx activate fish | source
 
