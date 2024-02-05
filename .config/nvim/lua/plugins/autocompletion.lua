2a9f701b08857bff85020a0690c06e14b6785912
Filipe Lemos <830213+filipeltsilva@users.noreply.github.com>
2024-02-05 19:22:40 -0300

    Refactoring Neovim autocompletion

--- .config/nvim/lua/plugins/autocompletion.lua
+++ .config/nvim/lua/plugins/autocompletion.lua
@@ -1,44 +1,27 @@
 return {
   "hrsh7th/nvim-cmp",
+  event = { "InsertEnter", "CmdlineEnter" },
   dependencies = {
     "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-nvim-lsp",
     "hrsh7th/cmp-nvim-lsp-signature-help",
     "hrsh7th/cmp-nvim-lua",
     "hrsh7th/cmp-path",
     "saadparwaiz1/cmp_luasnip",
 
-    -- Icons
     "onsails/lspkind.nvim",
 
-    -- Snippets
-    "L3MON4D3/LuaSnip",
+    { "L3MON4D3/LuaSnip", version = "v2.*", build = "make install_jsregexp" },
     "rafamadriz/friendly-snippets",
   },
-  event = { "InsertEnter" },
   config = function()
-    local cmp = require("cmp")
-    local lspkind = require("lspkind")
-    local luasnip = require("luasnip")
-
     require("luasnip.loaders.from_vscode").lazy_load()
 
-    luasnip.config.set_config({
-      enable_autosnippets = true,
-      history = true,
-      updateevents = "TextChanged,TextChangedI",
-    })
+    local cmp = require("cmp")
 
     cmp.setup({
-      confirm_opts = {
-        behavior = cmp.ConfirmBehavior.Replace,
-        select = true,
-      },
-      experimental = {
-        ghost_text = true,
-        native_menu = false,
-      },
       formatting = {
-        format = lspkind.cmp_format({
+        format = require("lspkind").cmp_format({
           ellipsis_char = "...",
           maxwidth = 50,
           mode = "symbol_text",
@@ -46,14 +29,14 @@ return {
         }),
       },
       mapping = cmp.mapping.preset.insert({
+        ["<C-h>"] = cmp.mapping.abort(),
         ["<C-j>"] = cmp.mapping.select_next_item(),
         ["<C-k>"] = cmp.mapping.select_prev_item(),
-
         ["<C-l>"] = cmp.mapping.confirm(),
       }),
       snippet = {
         expand = function(args)
-          luasnip.lsp_expand(args.body)
+          require("luasnip").lsp_expand(args.body)
         end,
       },
       sources = cmp.config.sources({
